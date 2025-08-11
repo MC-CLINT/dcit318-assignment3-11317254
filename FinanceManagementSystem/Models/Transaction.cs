e601e24f0baf47e698a3a3e1a9487553c33e0ab5
MC-CLINT <ckvidam@st.ug.edu.gh>
2025-08-11 23:27:38 +0000

    project setup

---
+++ FinanceManagementSystem/Models/Transaction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceManagementSystem.Models
+{
+    public record Transaction(
+        int Id,
+        DateTime Date,
+        decimal Amount,
+        string Category
+        );
+}
