161ddedd831e62bde130280f4d2cfaa6d104f306
MC-CLINT <ckvidam@st.ug.edu.gh>
2025-08-12 06:24:22 +0000

    Inventory Repository Added

--- WareHouseInventory/InventoryRepository.cs
+++ WareHouseInventory/InventoryRepository.cs
@@ -3,10 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WareHouseInventory.Interfaces;
 
 namespace WareHouseInventory
 {
-    internal class InventoryRepository
+    public class InventoryRepository<T> where T : IInventoryItem
     {
+        private Dictionary<int, T> _items = new Dictionary<int, T>();
+
+        public void AddItem(T item)
+        {
+            if (_items.ContainsKey(item.Id))
+                throw new DuplicateItemException($"Item with ID {item.Id} already exists.");
+
+            _items[item.Id] = item;
+        }
+
+        public T GetItemById(int id)
+        {
+            if (!_items.ContainsKey(id))
+                throw new ItemNotFoundException($"Item with ID {id} not found.");
+
+            return _items[id];
+        }
+
+        public void RemoveItem(int id)
+        {
+            if (!_items.ContainsKey(id))
+                throw new ItemNotFoundException($"Item with ID {id} not found.");
+
+            _items.Remove(id);
+        }
+
+        public List<T> GetAllItems()
+        {
+            return _items.Values.ToList();
+        }
+
+        public void UpdateQuantity(int id, int newQuantity)
+        {
+            if (newQuantity < 0)
+                throw new InvalidQuantityException("Quantity cannot be negative.");
+
+            if (!_items.ContainsKey(id))
+                throw new ItemNotFoundException($"Item with ID {id} not found.");
+
+            _items[id].Quantity = newQuantity;
+        }
     }
+
 }
