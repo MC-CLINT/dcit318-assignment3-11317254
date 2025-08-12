e84c817ce2275c43acbae1603c47eb3592a11aba
MC-CLINT <ckvidam@st.ug.edu.gh>
2025-08-12 05:17:43 +0000

    HealthCare Sytem

---
+++ HealthCareSystem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HealthCareSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
