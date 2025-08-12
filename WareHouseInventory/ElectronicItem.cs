98d32a4c5647546179e7d306a785412a6ac9646d
MC-CLINT <ckvidam@st.ug.edu.gh>
2025-08-12 06:15:19 +0000

    Grocery and Electronic Items Classes Added

---
+++ WareHouseInventory/ElectronicItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WareHouseInventory.Interfaces;
+
+namespace WareHouseInventory
+{
+    public class ElectronicItem : IInventoryItem
+    {
+        public int Id { get; }
+        public string Name { get; }
+        public int Quantity { get; set; }
+        public string Brand { get; }
+        public int WarrantyMonths { get; }
+
+        public ElectronicItem(int id, string name, int quantity, string brand, int warrantyMonths)
+        {
+            Id = id;
+            Name = name;
+            Quantity = quantity;
+            Brand = brand;
+            WarrantyMonths = warrantyMonths;
+        }
+    }
+}
---
+++ WareHouseInventory/GroceryItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WareHouseInventory.Interfaces;
+
+namespace WareHouseInventory
+{
+    public class GroceryItem : IInventoryItem
+    {
+        public int Id { get; }
+        public string Name { get; }
+        public int Quantity { get; set; }
+        public DateTime ExpiryDate { get; }
+
+        public GroceryItem(int id, string name, int quantity, DateTime expiryDate)
+        {
+            Id = id;
+            Name = name;
+            Quantity = quantity;
+            ExpiryDate = expiryDate;
+        }
+    }
+}
