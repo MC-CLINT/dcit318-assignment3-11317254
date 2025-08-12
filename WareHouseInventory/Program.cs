35931a0cd511eb5622da95886764592ec31b5bcb
MC-CLINT <ckvidam@st.ug.edu.gh>
2025-08-12 06:09:34 +0000

    IInventory

---
+++ WareHouseInventory/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WareHouseInventory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
