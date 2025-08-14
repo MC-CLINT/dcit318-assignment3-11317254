d9918a51aaa72d12de3e87b9059c2f29131df019
MC-CLINT <ckvidam@st.ug.edu.gh>
2025-08-14 15:06:39 +0000

    Inventory Records System

---
+++ InventoryRecordsSystem/InventoryItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryRecordsSystem
+{
+    namespace InventoryRecordsSystem
+    {
+        public record InventoryItem : IInventoryEntity
+        {
+            public int Id { get; init; }
+            public string Name { get; init; }
+            public int Quantity { get; init; }
+            public DateTime DateAdded { get; init; }
+
+        }
+    }
+}
---
+++ InventoryRecordsSystem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace InventoryRecordsSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
