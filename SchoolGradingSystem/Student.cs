ccbeae2ecfe9f75ab9f9540737e13f615d44cd8f
MC-CLINT <ckvidam@st.ug.edu.gh>
2025-08-14 11:42:49 +0000

    Student Class

---
+++ SchoolGradingSystem/Student.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolGradingSystem
+{
+    public class Student
+    {
+        public int Id { get;}
+        public string FullName { get;}
+        public int Score { get;}
+
+
+        public Student(int id, string fullName, int score)
+        {
+            Id = id;
+            FullName = fullName;
+            Score = score;
+        }
+
+        public string GetGrade()
+        {
+            if (Score >= 80 && Score <= 100)
+            {
+                return "A";
+            }
+            else if (Score >= 70)
+            {
+                return "B";
+            }
+            else if (Score >= 60)
+            {
+                return "C";
+            }
+            else if (Score >= 50)
+            {
+                return "D";
+            }
+            else
+                return "F";
+
+        }
+
+            public override string ToString() 
+        {
+            return $"ID: {Id}, FullName: {FullName}, Score: {Score}, Grade: {GetGrade()}";
+        }
+        }
+    }
+
--- SchoolGradingSystem/obj/Debug/net8.0/SchoolGradingSystem.AssemblyInfo.cs
+++ SchoolGradingSystem/obj/Debug/net8.0/SchoolGradingSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolGradingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66acce3d7a7faf6dce72d16d91768ca1615fbf23")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a85bed1eb3fa0fb1d2cfa196bc118eabc76837f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolGradingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolGradingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
