e6d2293f7457ac7a23ed9bc69d58d198aaf93eef
MC-CLINT <ckvidam@st.ug.edu.gh>
2025-08-14 13:26:21 +0000

    Working on main

--- SchoolGradingSystem/Program.cs
+++ SchoolGradingSystem/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string inputFilePath = "students.txt";
+
+            string outputFilePath = "report.txt";
+
+            var processor = new StudentResultProcessor();
+
+            try
+            {
+                List<Student> students = processor.ReadStudentsFromFile(inputFilePath);
+
+                processor.WriteReportToFile(students, outputFilePath);
+            }
+            catch (FileNotFoundException ex)
+            { 
+                Console.WriteLine("File Not Found"); 
+            }
+            catch (FormatException ex)
+            {
+
+            }
+
         }
     }
 }
