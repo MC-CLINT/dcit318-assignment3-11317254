bc47be2476201a20a8c7ab24aff2bc6d2c734d25
MC-CLINT <ckvidam@st.ug.edu.gh>
2025-08-14 11:44:24 +0000

    Exception files added

--- SchoolGradingSystem/obj/Debug/net8.0/SchoolGradingSystem.AssemblyInfo.cs
+++ SchoolGradingSystem/obj/Debug/net8.0/SchoolGradingSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolGradingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a85bed1eb3fa0fb1d2cfa196bc118eabc76837f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccbeae2ecfe9f75ab9f9540737e13f615d44cd8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolGradingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolGradingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
